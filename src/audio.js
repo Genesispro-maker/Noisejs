9c3a1bccd8d3516203e5e242d96d4543bb09f3b4
Genesispro-maker <genesisprog894@gmail.com>
2026-01-26 04:44:57 -0800

    Update audio.js

--- src/audio.js
+++ src/audio.js
@@ -1,33 +1,26 @@
-export default class Noises {
-    #audio
-    #context
-    #gainNode
-    #panner
-    #source
-    #loop
-    constructor({ src, volume = 1, pan = 0, loop = true}) {
-        this.#context = new AudioContext()
-        this.#audio = new Audio(src)
-        this.#loop = this.#audio.loop = loop;
-        this.#source = this.#context.createMediaElementSource(this.#audio)
-        this.#gainNode = this.#context.createGain()
-        this.#panner = new StereoPannerNode(this.#context)
-
-        this.#gainNode.gain.value = volume
-        this.#panner.pan.value = pan
-
-        this.#source
-            .connect(this.#panner)
-            .connect(this.#gainNode)
-            .connect(this.#context.destination)
+export default class Noise {
+    audioContext;
+    gainNode;
+    panner;
+    Source;
+    loop;
+    audioFile;
+    constructor({ src, volume = 1, pan = 0, loop = false }) {
+        this.audioContext = new AudioContext();
+        this.audioFile = new Audio(src);
+        this.loop = this.audioFile.loop = loop;
+        this.Source = this.audioContext.createMediaElementSource(this.audioFile);
+        this.gainNode = this.audioContext.createGain();
+        this.panner = new StereoPannerNode(this.audioContext);
+        this.gainNode.gain.value = volume;
+        this.panner.pan.value = pan;
+        this.Source.connect(this.panner).connect(this.gainNode).connect(this.audioContext.destination);
     }
-
     async play() {
-        await this.#context.resume()
-        this.#audio.play()
+        await this.audioContext.resume();
+        this.audioFile.play();
     }
-
-    pause(){
-        this.#audio.pause()
-    }   
+    pause() {
+        this.audioFile.pause();
+    }
 }
