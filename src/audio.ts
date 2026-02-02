3e4ad16336ad86e0b5ad87f149a4c5e73275dd85
Genesispro-maker <genesisprog894@gmail.com>
2026-02-02 14:06:33 -0800

    Update audio.ts

--- src/audio.ts
+++ src/audio.ts
@@ -1,9 +1,5 @@
-type AudioItem = {
-  id: number;
-  audio: HTMLAudioElement;
-};
-
 type NoiseType = {
+    src: string,
     volume: number,
     pan: number,
     loop: boolean,
@@ -29,21 +25,16 @@ export default class Noise{
     public audio: HTMLAudioElement
     public currentTime: string | undefined
 
-    constructor({src = [], volume = 1, pan = 0, loop = false}: Partial<NoiseType>){
+    constructor({src, volume = 1, pan = 0, loop = false}: Partial<NoiseType>){
         this.audioContext = new AudioContext()
-        this.audio = src.map<AudioItem>((audioFile, i) => ({
-            id: i,
-            audio: new Audio(audioFile),
-          }));
-
+        this.audio = new Audio(src)
         this.loop = this.audio.loop = loop
         this.currentTime = formatTime(this.audio.currentTime)
         this.Source = this.audioContext.createMediaElementSource(this.audio)
         this.gainNode = this.audioContext.createGain()
         this.panner = new StereoPannerNode(this.audioContext)
-       this.gainNode.gain.value =  volume
+        this.gainNode.gain.value =  volume
         this.panner.pan.value = pan
-
 
         
         this.Source.connect(this.panner).connect(this.gainNode).connect(this.audioContext.destination)
@@ -62,7 +53,7 @@ export default class Noise{
 }
 
 const noise = new Noise({
-    src: [],
+    src: "/plenty.mp3",
 })
 
 noise.play()
